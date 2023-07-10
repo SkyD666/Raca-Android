93bce2016c917a0cd91a56e0802c86277a31f547
SkyD666 <akarinnn666@gmail.com>
2023-07-10 11:37:23 +0800

    [optimize]优化SettingsItem写法

--- app/src/main/java/com/skyd/raca/ui/component/SettingsItem.kt
+++ app/src/main/java/com/skyd/raca/ui/component/SettingsItem.kt
@@ -1,22 +1,70 @@
 package com.skyd.raca.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.skyd.raca.ext.alwaysLight
+
+val LocalUseColorfulIcon = compositionLocalOf { false }
+val LocalVerticalPadding = compositionLocalOf { 16.dp }
+
+@Composable
+fun BannerItem(content: @Composable () -> Unit) {
+    Column {
+        Spacer(modifier = Modifier.height(16.dp))
+        CompositionLocalProvider(
+            LocalContentColor provides (LocalContentColor.current alwaysLight true),
+            LocalVerticalPadding provides 12.dp
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .clip(RoundedCornerShape(36))
+                    .background(MaterialTheme.colorScheme.primaryContainer alwaysLight true)
+            ) {
+                content()
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
 
 @Composable
 fun SwitchSettingsItem(
@@ -24,6 +72,7 @@ fun SwitchSettingsItem(
     text: String,
     description: String? = null,
     checked: Boolean = false,
+    enabled: Boolean = true,
     onCheckedChange: ((Boolean) -> Unit)?,
     onLongClick: (() -> Unit)? = null,
 ) {
@@ -32,6 +81,7 @@ fun SwitchSettingsItem(
         text = text,
         description = description,
         checked = checked,
+        enabled = enabled,
         onCheckedChange = onCheckedChange,
         onLongClick = onLongClick,
     )
@@ -43,19 +93,88 @@ fun SwitchSettingsItem(
     text: String,
     description: String? = null,
     checked: Boolean = false,
+    enabled: Boolean = true,
     onCheckedChange: ((Boolean) -> Unit)?,
     onLongClick: (() -> Unit)? = null,
 ) {
+    val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
     BaseSettingsItem(
+        modifier = Modifier.toggleable(
+            value = checked,
+            interactionSource = interactionSource,
+            indication = LocalIndication.current,
+            enabled = enabled,
+            role = Role.Switch,
+            onValueChange = { onCheckedChange?.invoke(it) },
+        ),
+        icon = icon,
+        text = text,
+        descriptionText = description,
+        enabled = enabled,
+        onLongClick = onLongClick,
+    ) {
+        Switch(
+            checked = checked,
+            enabled = enabled,
+            onCheckedChange = onCheckedChange,
+            interactionSource = interactionSource
+        )
+    }
+}
+
+@Composable
+fun RadioSettingsItem(
+    icon: ImageVector,
+    text: String,
+    description: String? = null,
+    selected: Boolean = false,
+    enabled: Boolean = true,
+    onLongClick: (() -> Unit)? = null,
+    onClick: (() -> Unit)? = null,
+) {
+    RadioSettingsItem(
+        icon = rememberVectorPainter(image = icon),
+        text = text,
+        description = description,
+        selected = selected,
+        enabled = enabled,
+        onLongClick = onLongClick,
+        onClick = onClick
+    )
+}
+
+@Composable
+fun RadioSettingsItem(
+    icon: Painter,
+    text: String,
+    description: String? = null,
+    selected: Boolean = false,
+    enabled: Boolean = true,
+    onLongClick: (() -> Unit)? = null,
+    onClick: (() -> Unit)? = null,
+) {
+    val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    BaseSettingsItem(
+        modifier = Modifier
+            .selectable(
+                selected = selected,
+                interactionSource = interactionSource,
+                indication = LocalIndication.current,
+                enabled = enabled,
+                role = Role.RadioButton,
+                onClick = { onClick?.invoke() },
+            ),
         icon = icon,
         text = text,
         descriptionText = description,
         onLongClick = onLongClick,
-        onClick = {
-            onCheckedChange?.invoke(!checked)
-        },
     ) {
-        Switch(checked = checked, onCheckedChange = onCheckedChange)
+        RadioButton(
+            selected = selected,
+            onClick = onClick,
+            enabled = enabled,
+            interactionSource = interactionSource
+        )
     }
 }
 
@@ -105,14 +224,17 @@ fun ColorSettingsItem(
 
 @Composable
 fun BaseSettingsItem(
+    modifier: Modifier = Modifier,
     icon: Painter,
     text: String,
     descriptionText: String? = null,
+    enabled: Boolean = true,
     onClick: (() -> Unit)? = null,
     onLongClick: (() -> Unit)? = null,
     content: (@Composable () -> Unit)? = null
 ) {
     BaseSettingsItem(
+        modifier = modifier,
         icon = icon,
         text = text,
         description = if (descriptionText != null) {
@@ -126,38 +248,51 @@ fun BaseSettingsItem(
                 )
             }
         } else null,
-        onClick = onClick,
-        onLongClick = onLongClick,
+        onClick = if (enabled) onClick else null,
+        onLongClick = if (enabled) onLongClick else null,
         content = content,
     )
 }
 
 @Composable
 fun BaseSettingsItem(
+    modifier: Modifier = Modifier,
     icon: Painter,
     text: String,
     description: (@Composable () -> Unit)? = null,
+    enabled: Boolean = true,
     onClick: (() -> Unit)? = null,
     onLongClick: (() -> Unit)? = null,
     content: (@Composable () -> Unit)? = null
 ) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .run {
-                if (onClick != null) combinedClickable(onLongClick = onLongClick) { onClick() }
-                else this
+                if (onClick != null && enabled) {
+                    combinedClickable(onLongClick = onLongClick) { onClick() }
+                } else this
             }
-            .padding(16.dp),
+            .padding(LocalVerticalPadding.current),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            modifier = Modifier
-                .padding(10.dp)
-                .size(24.dp),
-            painter = icon,
-            contentDescription = null
-        )
+        if (LocalUseColorfulIcon.current) {
+            Image(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(24.dp),
+                painter = icon,
+                contentDescription = null
+            )
+        } else {
+            Icon(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(24.dp),
+                painter = icon,
+                contentDescription = null,
+            )
+        }
         Column(
             modifier = Modifier
                 .weight(1f)
@@ -196,4 +331,15 @@ fun CategorySettingsItem(text: String) {
         maxLines = 3,
         overflow = TextOverflow.Ellipsis,
     )
+}
+
+@Composable
+fun TipSettingsItem(text: String) {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp + 10.dp, vertical = 10.dp)
+    ) {
+        Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = text, style = MaterialTheme.typography.bodyMedium)
+    }
 }
